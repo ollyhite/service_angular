a1c1518daa139a44c1e95eb1d5399e635a0fd71f
PeiYun <ollylee8520@gmail.com>
2024-03-23 21:10:09 -0600

    cross communicte from service func

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -24,6 +24,8 @@ export class AccountComponent {
   onSetTo(status: string) {
     // this.statusChanged.emit({ id: this.id, newStatus: status });
     this.accountService.updateStatus(this.id, status);
+    this.accountService.statusUpdated.emit(status);
+
     // this.loggingService.logStatusChange(status);
 
     // console.log('A server status changed, new status: ' + status);
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -15,7 +15,11 @@ export class NewAccountComponent {
   constructor(
     // private loggingService: LoggingService,
     private accountService: AccountService
-  ) {}
+  ) {
+    this.accountService.statusUpdated.subscribe((status: string) =>
+      alert('New Status:' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     // this.accountAdded.emit({
